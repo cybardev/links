acc5cc43f0c6ed0fc4957a328117d0f362f98f45
cybardev <sheikh@cybar.dev>
2024-02-20 15:49:02 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# links
+
+## Social media landing page
+
+Linktree, but mine.
---
+++ public/links.js
@@ -0,0 +1,13 @@
+const data = {
+    name: "Sheikh",
+    handle: "@cybardev",
+    tagline: "Software Developer | Pixel Artist | Computer Science Student",
+    location: "Halifax, NS, Canada",
+    links: {
+        GitHub: "https://github.com/cybardev",
+        Linkedin: "https://linkedin.com/in/cybardev",
+        Resume: "https://resume.cybar.dev",
+        Blog: "https://blog.cybar.dev",
+        Instagram: "https://instagram.com/cybardev",
+    },
+};
