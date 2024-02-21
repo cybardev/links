2075ee9ca5b997027d0bc7803a539768032bee1c
cybardev <sheikh@cybar.dev>
2024-02-21 00:09:06 -0400

    re: use vanilla JS instead of AlpineJS

---
+++ public/script.js
@@ -0,0 +1,24 @@
+// alias to retrieve DOM objects
+const $ = (selector) => document.querySelector(selector);
+
+window.onload = () => {
+    // header
+    $("#name").innerText = data.name;
+    $("#handle").innerText = data.handle;
+    $("#tagline").innerText = data.tagline;
+
+    // links
+    const links = $("#links");
+    for (const [site, url] of Object.entries(data.links)) {
+        links.insertAdjacentHTML(
+            "beforeend",
+            `<a href="${url}" class="link">${site}</a>`
+        );
+    }
+
+    // footer
+    $("#footer-text").insertAdjacentHTML(
+        "beforeend",
+        `<a href="${data.links.GitHub}">${data.handle}</a>`
+    );
+};
